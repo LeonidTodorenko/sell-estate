1d7f47c07002651d83e49f89fedc51473dc54648
Leonid Todorenko <todor.leo@gmail.com>
2025-07-11 18:23:01 +0300

    багофикс

--- RealEstateInvestment/Program.cs
+++ RealEstateInvestment/Program.cs
@@ -91,11 +91,21 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+//using (var scope = app.Services.CreateScope())
+//{
+//    var superUserService = scope.ServiceProvider.GetRequiredService<ISuperUserService>();
+//    await superUserService.EnsureSuperUserExistsAsync();
+//}
+
 using (var scope = app.Services.CreateScope())
 {
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.Migrate(); 
+
     var superUserService = scope.ServiceProvider.GetRequiredService<ISuperUserService>();
     await superUserService.EnsureSuperUserExistsAsync();
 }
+
 
 using (var scope = app.Services.CreateScope())
 {
